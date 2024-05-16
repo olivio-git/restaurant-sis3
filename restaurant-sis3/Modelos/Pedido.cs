b8d06d8b53ce02737b749d65f2e99bb77f4ad59f
olivio-git <subelzaolivitocabezas@gmail.com>
2024-05-15 20:59:09 -0400

    update

---
+++ restaurant-sis3/Modelos/Pedido.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace restaurant_sis3.Modelos;
+
+public partial class Pedido
+{
+    [Key]
+    [Column("PedidoID")]
+    public int PedidoId { get; set; }
+
+    [Column("ClienteID")]
+    public int? ClienteId { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? FechaPedido { get; set; }
+
+    [Column(TypeName = "decimal(10, 2)")]
+    public decimal? Total { get; set; }
+
+    [StringLength(50)]
+    public string? Estado { get; set; }
+
+    [Column("PromocionID")]
+    public int? PromocionId { get; set; }
+
+    [Column("DescuentoID")]
+    public int? DescuentoId { get; set; }
+
+    [Column("UsuarioID")]
+    public int? UsuarioId { get; set; }
+
+    [ForeignKey("ClienteId")]
+    [InverseProperty("Pedidos")]
+    public virtual Cliente? Cliente { get; set; }
+
+    [ForeignKey("DescuentoId")]
+    [InverseProperty("Pedidos")]
+    public virtual Descuento? Descuento { get; set; }
+
+    [InverseProperty("Pedido")]
+    public virtual ICollection<DetallesPedido> DetallesPedidos { get; set; } = new List<DetallesPedido>();
+
+    [ForeignKey("PromocionId")]
+    [InverseProperty("Pedidos")]
+    public virtual Promocione? Promocion { get; set; }
+
+    [ForeignKey("UsuarioId")]
+    [InverseProperty("Pedidos")]
+    public virtual Usuario? Usuario { get; set; }
+}
---
+++ restaurant-sis3/Modelos/Producto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace restaurant_sis3.Modelos;
+
+public partial class Producto
+{
+    [Key]
+    [Column("ProductoID")]
+    public int ProductoId { get; set; }
+
+    [StringLength(100)]
+    public string? Nombre { get; set; }
+
+    [Column(TypeName = "decimal(10, 2)")]
+    public decimal? Precio { get; set; }
+
+    [Column("CategoriaID")]
+    public int? CategoriaId { get; set; }
+
+    [Column("ProveedorID")]
+    public int? ProveedorId { get; set; }
+
+    [Column("InventarioID")]
+    public int? InventarioId { get; set; }
+
+    [ForeignKey("CategoriaId")]
+    [InverseProperty("Productos")]
+    public virtual Categoria? Categoria { get; set; }
+
+    [InverseProperty("Producto")]
+    public virtual ICollection<DetallesPedido> DetallesPedidos { get; set; } = new List<DetallesPedido>();
+
+    [InverseProperty("Producto")]
+    public virtual ICollection<Ingrediente> Ingredientes { get; set; } = new List<Ingrediente>();
+
+    [ForeignKey("InventarioId")]
+    [InverseProperty("Productos")]
+    public virtual Inventario? Inventario { get; set; }
+
+    [ForeignKey("ProveedorId")]
+    [InverseProperty("Productos")]
+    public virtual Proveedore? Proveedor { get; set; }
+}
